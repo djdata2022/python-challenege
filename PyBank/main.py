db09a7351b33e66f4abaa0a0a47e7a011dbe226c
djdata2022 <120222002+djdata2022@users.noreply.github.com>
2023-01-24 20:02:33 -0500

    final edits

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,64 +2,61 @@
 import os
 import csv
 
-#create variables to process data file
+#create file path variables to process data file
 file_path = os.path.join("Resources","budget_data.csv")
 output_file_path = os.path.join("Analysis","output_report.txt")
 
-#create empty lists to hold the values/columns in the data file
+#create empty lists to hold the values in the data file
 date_list = []
 profit_list = []
 profit_diff = []
 
-#create variable for total profit and total profit difference and set to zero
-profit_sum = 0
-profit_diff_sum = 0
-
 #open data file and read data line by line
 with open(file_path, "r") as csv_file:
     csv_reader =csv.reader(csv_file, delimiter=",")
-    next(csv_reader)
+    #save header row as requested
+    header_row = next(csv_reader)
 
     #iterate csv file from second row through to the end, adding dates
-    #and profits to new lists
+    #and profits to created lists
     for row in csv_reader:
         date_list.append(row[0])
         profit_list.append(int(row[1]))
-        #calculate the sum of profit
-        #profit_sum = profit_sum + int(row[1])
 
-profit_sum = sum(profit_list) #**could also do this after making list integer?**
+#calculate sum of profit
+profit_sum = sum(profit_list)
 
 #calculate the number of months
 num_months = len(date_list)
 
-#calculate profit difference between items in list
+#calculate profit difference between items in list, place in profit_diff list
 for n in range(1, len(profit_list)):
     profit_diff.append((profit_list[n]) - (profit_list[n-1]))
 
-#Calc av profit loss, max and min
+#calculate average profit loss, max and min
 av_profit_loss = sum(profit_diff)/len(profit_diff)
 max_profit = max(profit_diff)
 min_profit = min(profit_diff)
 
-#combine dates and profit change in one file
-combined_file = zip(date_list,profit_diff)
+#use index function to determine index for max and min; use to find corresponding dates
+max_index = 1+profit_diff.index(max_profit)
+min_index = 1+profit_diff.index(min_profit)
 
-#create variable to hold report ***2 DP, ADD DATES FOR MAX, MIN**
+#create variable to hold report
 output_report = (
 f"Financial Analysis \n" 
 f"____________________________________ \n"
 
 f"Total Months: {num_months} \n"
-f"Total: ${profit_sum}\n"
+f"Total: ${profit_sum} \n"
 f"Average Change: ${av_profit_loss:.2f} \n"
-f"Greatest Increase in Profits: {date_list[index]} ${max_profit}\n"
-f"Greatest Decrease in Profits: ${min_profit}\n"
+f"Greatest Increase in Profits: {date_list[max_index]} (${max_profit}) \n"
+f"Greatest Decrease in Profits: {date_list[min_index]} (${min_profit}) \n"
 )
 
 #print analysis to terminal
 print(output_report)
 
-#export text file with results **REMOVE COMMENTS BELOW BEFORE SUBMITTING**
-#with open(output_file_path,"w") as txt_file:
-#    txt_file.write(output_report)
+#export text file with results
+with open(output_file_path,"w") as txt_file:
+    txt_file.write(output_report)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,36 +2,29 @@
 import os
 import csv
 
-#create variables to process data file
+#create file paths to process data file
 file_path = os.path.join("Resources","election_data.csv")
 output_file_path = os.path.join("Analysis","output_report.txt")
 
-#create empty lists to hold the values/columns in the data file
+#create empty lists to hold the values in the data file
 voter_list = []
 county_list = []
 candidate_list = []
 
-#create variable for total profit and total profit difference and set to zero
-#profit_sum = 0
-#profit_diff_sum = 0
-
 #open data file and read data line by line
 with open(file_path, "r") as csv_file:
     csv_reader =csv.reader(csv_file, delimiter=",")
-    next(csv_reader)
+    #save header row as requested
+    header_row = next(csv_reader)
 
     #iterate csv file from second row through to the end, adding items to lists
     for row in csv_reader:
         voter_list.append(row[0])
         county_list.append(row[1])
         candidate_list.append(row[2])
-        #calculate the sum of profit
-        #profit_sum = profit_sum + int(row[1])
 
 #calculate total number of votes cast
 total_votes=len(voter_list)
-
-print(total_votes)
 
 #create complete list of candidates who received votes
 unique_candidates = []
@@ -39,8 +32,6 @@ unique_candidates = []
 for item in candidate_list:
     if item not in unique_candidates:
         unique_candidates.append(item)
-
-print(unique_candidates)
 
 #create empty list for counts
 votes_per_candidate = []
@@ -50,8 +41,6 @@ for candidates in unique_candidates:
     count=candidate_list.count(candidates)
     votes_per_candidate.append(count)
 
-print(votes_per_candidate) # remove before final
-
 #create empty list to hold vote percentages
 percent_vote = []
 
@@ -59,27 +48,23 @@ percent_vote = []
 for x in votes_per_candidate:
     percentage = 100*int(x)/int(total_votes)
     percent_vote.append(percentage)
-    
-print(percent_vote) # remove before final
-
-#create combined file of summary results - candidate, votes, percent
-combined_file = zip(unique_candidates,votes_per_candidate, percent_vote)
-
-print(combined_file) #remove before finalizing
 
 winner_index = votes_per_candidate.index(max(votes_per_candidate))
 
-
 #create variable to hold report
 output_report = (
+f" \n"
 f"Election Results \n"
 f"____________________________________ \n"
+f" \n"
 f"Total Votes: {total_votes} \n"
 f"____________________________________ \n"
+f" \n"
 f"{unique_candidates[0]}: {percent_vote[0]:.3f}% ({votes_per_candidate[0]}) \n"
 f"{unique_candidates[1]}: {percent_vote[1]:.3f}% ({votes_per_candidate[1]}) \n"
 f"{unique_candidates[2]}: {percent_vote[2]:.3f}% ({votes_per_candidate[2]}) \n"
 f"____________________________________ \n"
+f" \n"
 f"Winner: {unique_candidates[winner_index]} \n"
 f"____________________________________ \n"
 )
@@ -87,16 +72,6 @@ f"____________________________________ \n"
 #print analysis to terminal
 print(output_report)
 
-#for row in combined_file:
-#    print(
-#f"{row[0]} ": " {row[1]} \n"
-#f"Average Change: ${av_profit_loss}\n"
-#f"Greatest Increase in Profits: ${max_profit}\n"
-#f"Greatest Decrease in Profits: ${min_profit}\n"
-
-
-
-
-#export text file with results **REMOVE COMMENTS BELOW BEFORE SUBMITTING**
-#with open(output_file_path,"w") as txt_file:
-#    txt_file.write(output_report)
+#export text file with results
+with open(output_file_path,"w") as txt_file:
+    txt_file.write(output_report)
